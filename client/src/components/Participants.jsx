1112366f0b849e0f28f9fcfed6ee2d8188bc851c
divyansraj <divyanshu10245@gmail.com>
2024-11-04 02:13:30 +0530

    third-commit

--- client/src/components/Participants.jsx
+++ client/src/components/Participants.jsx
@@ -1,29 +1,17 @@
-import { useEffect, useState } from "react";
-import UserAvatar from "react-user-avatar";
-//import { setTotalJoined } from "../utils/RoomSlice";
-import { useDispatch } from "react-redux";
+//import UserAvatar from "react-user-avatar";
+import Avatar from "@mui/material/Avatar";
 const Participants = ({ clients = [] }) => {
-  //const dispatch = useDispatch();
-  const [length,setLength] = useState(0);
-  const getCount =()=>{
-    let userCount =0;
-    clients.forEach((client) => {
-      if(client.username){
-        userCount+=1;
-      }
-    });
-    return userCount;
-  }
-  useEffect(()=>{
-    getCount(setLength());
-    console.log(length);
-  },[clients])
+
 
   return (
-     <div className="flex flex-row flex-wrap gap-5 p-4 text-white font-semibold">
+    <div className="flex flex-row flex-wrap gap-5 p-4 text-white font-semibold">
       {clients.map((client, index) => (
         <div key={index} className="flex flex-col justify-center items-center">
-          <UserAvatar size="50" name={client.username} />
+          <Avatar
+          alt={client.username}
+          size="50"
+            name={client.username}
+          />
           <h1>{client.username}</h1>
         </div>
       ))}
