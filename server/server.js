fcbe71240d0b7ee809d02c40aa935fd6a4b2ae04
divyansraj <divyanshu10245@gmail.com>
2024-11-04 03:29:43 +0530

    fifth(3)-commit

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const { Server } = require("socket.io");
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://code-share-ten.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://code-share-ten.vercel.app",
+      "https://code-share-backend-one.vercel.app",
+    ],
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -113,7 +117,11 @@ socket.on("fontsize", ({ roomId, fontSize }) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://code-share-ten.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://code-share-ten.vercel.app",
+      "https://code-share-backend-one.vercel.app",
+    ],
     methods: ["GET", "POST"],
     credentials: true,
   })
